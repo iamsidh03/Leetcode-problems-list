6c09ef68bb11f6bcbbff56d5c34c008cfcf04a41
SIDDHARTH RAJ <118841155+iamsidh03@users.noreply.github.com>
2025-09-07 16:15:24 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (74.03%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,15 +1,22 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(begin(t),end(t));
-        sort(begin(s),end(s));
+        //without sorting O(N)
         int slen=s.length();
         int tlen=t.length();
-        if(slen!=tlen)return false;
+        if(slen!=tlen) return false;
+        vector<int>sfreq(26,0),tfreq(26,0);
         for(int i=0;i<slen;i++){
-            if(s[i]!=t[i]){
-                return false;
-            }
+            int sidx=s[i]-'a';
+            int tidx=t[i]-'a';
+            sfreq[sidx]++;
+            tfreq[tidx]++;
+
+        }
+        //ligic is that if they are anagram
+        // the frequescy of each character must be same at that particular index
+        for(int i=0;i<26;i++){
+            if(sfreq[i]!=tfreq[i])return false;
         }
         return true;
     }
