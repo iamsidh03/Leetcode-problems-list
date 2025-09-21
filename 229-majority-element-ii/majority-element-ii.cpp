f00106dd450cf425cbf0165a461c0e0ac6592352
SIDDHARTH RAJ <118841155+iamsidh03@users.noreply.github.com>
2025-09-22 02:27:24 +0530

    Time: 3 ms (54.63%) | Memory: 23.6 MB (62.47%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,18 +1,44 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-       unordered_map<int,int>hash;
-    vector<int>ans;
-       int n=nums.size();
-       for(int i=0;i<n;i++){
-             hash[nums[i]]++;
+        //using Morse algo
+        int n=nums.size();
+        int cnt1=0;
+        int cnt2=0;
+        int mjt1=0,mjt2=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]==mjt1){
+                cnt1++;
+            }else if(nums[i]==mjt2){
+                cnt2++;
+            }else if(cnt1==0){
+                mjt1=nums[i];
+                cnt1=1;
+            }else if(cnt2==0){
+                mjt2=nums[i];
+                cnt2=1;
+            }else{
+                cnt1--;
+                cnt2--;
+            }
         }
-       int majority=floor(n/3);
-       for(auto &p:hash){
-        if(p.second>majority){
-            ans.push_back(p.first);
+        // cout<<mjt1<<mjt2;
+        vector<int>ans;
+        int freq1=0;
+        int freq2=0;
+        for(int i:nums){
+            if(mjt1==i) freq1++;
+            else if(mjt2==i) freq2++;
         }
-       }
-       return ans;
+            if(freq1>floor(n/3)){
+                ans.push_back(mjt1);
+            }
+            if(freq2>floor(n/3)){
+                ans.push_back(mjt2);
+            }
+        
+            return ans;
+        
+
     }
 };
