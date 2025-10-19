8abad3f1f19ff3745051dd7ed7a9ba7f4260d194
SIDDHARTH RAJ <118841155+iamsidh03@users.noreply.github.com>
2025-10-19 14:18:42 +0530

    Time: 116 ms (18.85%) | Memory: 183.8 MB (24.75%) - LeetSync

---
+++ 146-lru-cache/lru-cache.cpp
@@ -0,0 +1,53 @@
+/* 
+when we erase in our bruteforce approach it take O(n) in vector,
+if we use DDL then the erase time is O(1).
+also we can store the address of each node in hashtable for faster lookup time.
+
+ */
+
+class LRUCache {
+public:
+    list<int> dll;
+    map<int,pair<list<int>::iterator,int>>mp;
+    int n;
+    LRUCache(int capacity) {
+        n=capacity;
+    }
+    void makeRecentlyUsed(int key){
+            dll.erase(mp[key].first);
+            dll.push_front(key);
+            mp[key].first=dll.begin();
+    }
+    int get(int key) {
+        if(mp.find(key)==mp.end()){
+            return -1;
+        }
+        makeRecentlyUsed(key);
+        return mp[key].second;
+    }
+    
+    void put(int key, int value) {
+        if(mp.find(key)!=mp.end()){
+            mp[key].second=value;
+            makeRecentlyUsed(key);
+
+        }else{
+            dll.push_front(key);
+            mp[key]={dll.begin(),value};
+            n--;
+        }
+        if(n<0){
+            int key_to_deleted=dll.back();
+            mp.erase(key_to_deleted);
+            dll.pop_back();
+            n++;
+        }
+    }
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
