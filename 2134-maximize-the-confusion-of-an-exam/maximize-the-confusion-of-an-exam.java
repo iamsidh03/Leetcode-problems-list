b932af068efef80d6b7df10e2082d95ab8a9e898
SIDDHARTH RAJ <118841155+iamsidh03@users.noreply.github.com>
2026-02-10 02:46:58 +0530

    Time: 13 ms (61.44%) | Memory: 46.1 MB (95.71%) - LeetSync

--- 2134-maximize-the-confusion-of-an-exam/maximize-the-confusion-of-an-exam.java
+++ 2134-maximize-the-confusion-of-an-exam/maximize-the-confusion-of-an-exam.java
@@ -1,27 +1,36 @@
 class Solution {
-    private int solve(String s,int k,char ch){
-        int left=0;
-        
-        int maxlen=0;
-        int cnt=0;
-        for(int right=0;right<s.length();right++){
-            
-            if(s.charAt(right)==ch){
-                cnt++;
-            }
-            while(cnt>k){
-              if(s.charAt(left)==ch){
-                cnt--;
-            }  
-            left++;
-            }
-            maxlen=Math.max(maxlen,right-left+1);
-        }
-        return maxlen;
-    }
     public int maxConsecutiveAnswers(String answerKey, int k) {
-        int maxTchange=solve(answerKey,k,'T');
-        int maxFchange=solve(answerKey,k,'F');
-        return Math.max(maxTchange,maxFchange);
+
+        int left = 0;
+        int maxLen = 0;
+
+        int countT = 0;
+        int countF = 0;
+
+        for (int right = 0; right < answerKey.length(); right++) {
+
+            // Expand window
+            if (answerKey.charAt(right) == 'T') {
+                countT++;
+            } else {
+                countF++;
+            }
+
+            // If more than k changes are required, shrink window
+            while ((right - left + 1) - Math.max(countT, countF) > k) {
+
+                if (answerKey.charAt(left) == 'T') {
+                    countT--;
+                } else {
+                    countF--;
+                }
+                left++;
+            }
+
+            // Update max length
+            maxLen = Math.max(maxLen, right - left + 1);
+        }
+
+        return maxLen;
     }
 }
