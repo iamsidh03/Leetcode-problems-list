b56bf2f6b08e3b4f66476a2816024bfe4a166a8d
SIDDHARTH RAJ <118841155+iamsidh03@users.noreply.github.com>
2026-02-12 21:02:16 +0530

    Time: 10 ms (87.86%) | Memory: 59.6 MB (51.01%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
@@ -1,16 +1,49 @@
+/*
 class Solution {
     public int numberOfSubarrays(int[] nums, int k) {
-        int n = nums.length;
-        int[] cnt = new int[n + 1];
-        cnt[0] = 1;
-        int ans = 0, t = 0;
-        for (int v : nums) {
-            t += v & 1;
-            if (t - k >= 0) {
-                ans += cnt[t - k];
+        int n=nums.length;
+        int finalcnt=0;
+        for(int i=0;i<n;i++){
+            int cnt=0;
+            for(int j=i;j<n;j++){
+                if((nums[j]&1)==1){
+                    cnt++;
+                }
+                if(cnt==k){
+                    finalcnt++;
+                    
+                }
             }
-            cnt[t]++;
         }
-        return ans;
+        return finalcnt;
+    }
+}
+*/
+class Solution {
+
+    private int atMost(int[] nums, int k) {
+        int st = 0;
+        int count = 0;
+        int oddcnt = 0;
+
+        for (int end = 0; end < nums.length; end++) {
+
+            if ((nums[end] & 1) == 1)
+                oddcnt++;
+
+            while (oddcnt > k) {
+                if ((nums[st] & 1) == 1)
+                    oddcnt--;
+                st++;
+            }
+
+            count += end - st + 1;
+        }
+
+        return count;
+    }
+
+    public int numberOfSubarrays(int[] nums, int k) {
+        return atMost(nums, k) - atMost(nums, k - 1);
     }
 }
