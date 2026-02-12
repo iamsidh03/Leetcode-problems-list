6773c7d03075bd421f911124d4818b8e4d7160ba
SIDDHARTH RAJ <118841155+iamsidh03@users.noreply.github.com>
2026-02-12 16:14:52 +0530

    Time: 3 ms (99.05%) | Memory: 81.2 MB (85.36%) - LeetSync

---
+++ 2478-longest-nice-subarray/longest-nice-subarray.java
@@ -0,0 +1,50 @@
+/*
+class Solution {
+    private boolean  isNice(int []nums,int st,int end){
+        int mask=0;
+
+        for(int i=st;i<=end;i++){
+            if((mask&nums[i])!=0){
+                return false;
+            }
+            mask|=nums[i];
+        }
+        return true;
+    }
+    public int longestNiceSubarray(int[] nums) {
+            int n=nums.length;
+            int maxLen=0;
+        for (int i = 0; i < n; i++) {
+            int len=1;
+            for (int j = i + 1; j < n; j++) {
+                if (isNice(nums, i, j))
+                   len=j-i+1;
+                else
+                    break;
+            }
+            maxLen=Math.max(maxLen,len);
+        }
+        return maxLen;
+    }
+}
+*/
+
+class Solution {
+
+    public int longestNiceSubarray(int[] nums) {
+        int n = nums.length;
+        int maxLen = 0;
+        for (int i = 0; i < n; i++) {
+            int mask = 0;
+            for (int j = i; j < n; j++) {
+                if ((mask & nums[j]) != 0)
+                    break;
+                mask |= nums[j];
+                maxLen = Math.max(maxLen, j - i + 1);
+
+            }
+
+        }
+        return maxLen;
+    }
+}
