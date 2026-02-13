51c6ec4bb500e8133b7251087d146ad1f0ccbde3
SIDDHARTH RAJ <118841155+iamsidh03@users.noreply.github.com>
2026-02-13 19:59:43 +0530

    Time: 9 ms (46.93%) | Memory: 43.9 MB (94.11%) - LeetSync

---
+++ 567-permutation-in-string/permutation-in-string.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        if(s1.length()>s2.length())return false;
+        
+        int s1arr[]=new int[26];
+        int s2arr[]=new int[26];
+
+        for(int i=0;i<s1.length();i++){
+            s1arr[s1.charAt(i)-'a']++;
+            s2arr[s2.charAt(i)-'a']++;
+        }
+
+        for(int i=0;i<s2.length()-s1.length();i++){
+            if(match(s1arr,s2arr)){
+                return true;
+            }
+            s2arr[s2.charAt(i+s1.length())-'a']++;
+            s2arr[s2.charAt(i)-'a']--;
+        }
+        return match(s1arr,s2arr);
+    }
+    private  boolean match(int []arr1,int []arr2){
+        for(int i=0;i<26;i++){
+            if(arr1[i]!=arr2[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
