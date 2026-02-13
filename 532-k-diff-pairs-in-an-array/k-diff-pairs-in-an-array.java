2a134e89b2b3e8cdfbf16dcc302b2754a670dd8c
SIDDHARTH RAJ <118841155+iamsidh03@users.noreply.github.com>
2026-02-14 02:22:33 +0530

    Time: 11 ms (77.35%) | Memory: 45.9 MB (98.83%) - LeetSync

--- 532-k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.java
+++ 532-k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.java
@@ -1,27 +1,25 @@
 class Solution {
     public int findPairs(int[] nums, int k) {
-        // Set<String> seen =new HashSet<>();
-        Arrays.sort(nums); 
-        int cnt=0;
-        for(int i=0;i<nums.length;i++){
-            if(i>0 && nums[i]==nums[i-1]) continue;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int n : nums) {
+            map.put(n, map.getOrDefault(n, 0) + 1);
+        }
+        int cnt = 0;
 
-            int tar=nums[i]+k;
-            if(bs(nums,i+1,nums.length-1,tar)){
-                cnt++;
+        if (k == 0) {
+            //freq >1
+
+            for (int freq : map.values()) {
+                if (freq > 1)
+                    cnt++;
             }
-           
+        } else {
+            for (int n : map.keySet()) {
+                if (map.containsKey(n + k)) {
+                    cnt++;
+                }
+            }
         }
         return cnt;
-    }
-    private boolean bs(int []nums,int left,int right,int target){
-        while(left<=right){
-            int mid=left+(right-left)/2;
-
-            if(nums[mid]==target) return true;
-            else if(nums[mid]<target) left=mid+1;
-            else right=mid-1;
-        }
-        return false;
     }
 }
